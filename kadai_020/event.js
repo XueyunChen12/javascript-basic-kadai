a9123bd893857ee850f2dfb0955dfe34d0c75104
Yuki <xchendaito@gmail.com>
2023-12-28 20:15:26 +0900

    kadai20

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,9 @@
 //HTML要素をidで取得し、中身を出力する
 const text = document.getElementById('text');
 
+//HTML要素をidで取得し、中身を出力する
+btn text = document.getElementById('btn');
+
 //HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
   document.getElementById('text') .textContent= 'ボタンをクリックしました';
